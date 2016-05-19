619d52e2bd97da5002d02344860567973d15be01
Thibault BRUHIERE <tbruhiere@gmail.com>
2016-05-19 15:47:01 +0200

    Create the Ikeo App
    In this file, we :
      - set the main module of Nods.JS (express, ...)
      - set the channel of the node server

---
+++ IkeoApp.js
@@ -0,0 +1,52 @@
+// Call all the node's modules we need for the project
+// ----------------------------------------------------------------
+var express = require('express');
+var bodyParser = require('body-parser');
+var logConsole = require('extended-console');
+
+
+// Call the node's module proper to the "IkeoWordl" Project (controllers)
+// -----------------------------------------------------------------------------
+
+// 2 JSON Object :
+var franckyFamilyController = require('./controllers/JSON/franckyFamily.js');
+var worldController = require('./controllers/JSON/world.js')
+
+//
+
+// Path to the website View
+
+
+
+
+
+
+//
+//
+//  > Start programming
+// -----------------------------------------------------------------------------
+
+// Define square path
+var IkeoApp = express();
+IkeoApp.use(express.static(__dirname + '/public'));
+
+// Set the Ikeo World
+var ObjIkeoWorld = worldController.addNewWorld(0, '', 0, 0, '');
+ObjIkeoWorld[1] = worldController.addNewWorld(1, 'Ikeo', 15, 20, '#4CAF50');
+
+// Create the object and set one member of the francky family
+var ObjFranckyFamily = franckyFamilyController.addNewFrancky(0, '', '', '', 0, 0, '');
+ObjFranckyFamily[1] = franckyFamilyController.addNewFrancky(1, 'Franck', 'mood', '#607D8B', 1, 1, 'North');
+
+
+//console.log(ObjFranckyFamily[1].name);
+//console.log(ObjIkeoWorld[1]);
+
+
+
+//
+//
+//  > Set server on 10000 channel
+// -----------------------------------------------------------------------------
+IkeoApp.listen(10000);
+module.export = IkeoApp;
