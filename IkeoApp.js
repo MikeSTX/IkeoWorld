6767dcc7f7c8df52b43cda169a3e3878b763c0f0
Thibault BRUHIERE <tbruhiere@gmail.com>
2016-05-19 15:53:54 +0200

    Create the JSON object
    Let's define the parameters of the JSON objects ;
      - franckyFamily (to create/add a new francky)
      - world (to create a new world - Ikeo)

--- IkeoApp.js
+++ IkeoApp.js
@@ -14,11 +14,34 @@ var logConsole = require('extended-console');
 //  > Start programming
 // -----------------------------------------------------------------------------
 
-// Define square path
+// Define root path
 var IkeoApp = express();
 IkeoApp.use(express.static(__dirname + '/public'));
 
-console.log('bonjour');
+
+// Call the node's module proper to the "IkeoWordl" Project (controllers)
+// -----------------------------------------------------------------------------
+// 2 JSON Object :
+var franckyFamilyController = require('./controllers/JSON/franckyFamily.js');
+var worldController = require('./controllers/JSON/world.js')
+
+//
+
+// Path to the website View
+
+
+
+// Set the Ikeo World
+var ObjIkeoWorld = worldController.addNewWorld(0, '', 0, 0, '');
+ObjIkeoWorld[1] = worldController.addNewWorld(1, 'Ikeo', 15, 20, '#4CAF50');
+
+// Create the object and set one member of the francky family
+var ObjFranckyFamily = franckyFamilyController.addNewFrancky(0, '', '', '', 0, 0, '');
+ObjFranckyFamily[1] = franckyFamilyController.addNewFrancky(1, 'Franck', 'mood', '#607D8B', 1, 1, 'North');
+
+
+//console.log(ObjFranckyFamily[1].name);
+//console.log(ObjIkeoWorld[1]);
 
 
 
