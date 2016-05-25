77e9e2c9d666ee4c6956a14a3c716dc3b5857778
Thibault BRUHIERE <tbruhiere@gmail.com>
2016-05-25 15:07:06 +0200

    Version 1.0

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 // Call all the node's modules we need for the project
 // ----------------------------------------------------------------
 var express = require('express');
-var bodyParser = require('body-parser');
 var logConsole = require('extended-console');
-
-//var EventEmitter = require('events').EventEmitter;
-
-
 
 
 //
@@ -17,16 +12,6 @@ var logConsole = require('extended-console');
 // Define root path
 var app = express();
 app.use(express.static(__dirname + '/libraries'));
-
-/*var keypress = new EventEmitter();
-keypress.on('event', function() {
-    console.log('bonjour');
-});
-
-
-keypress.emit('event');*/
-
-
 
 
 
@@ -43,10 +28,6 @@ var homeController = require('./controllers/homeFactory')
 var explorerController = require('./controllers/explorerFactory')
 
 
-
-
-
-
 //
 // Create objects
 // -----------------------------------------------------------------------------
@@ -58,9 +39,6 @@ var FranckyFamily = [
   ]
 
 
-exports.FranckyFamily = FranckyFamily;
-
-
 
 // Define path of the websiet view
 // --------------------------------------------
@@ -69,65 +47,6 @@ app.get('/home', homeController.homeFactory);
 
 app.get('/explorer/', explorerController.explorerFactory(FranckyFamily, IkeoWorld));
 app.get('/explorer/:idFrancky', explorerController.franckyFocus(FranckyFamily, IkeoWorld));
-
-
-//
-// Wait for event of the keypress
-//
-//  <> 3 commands
-//    > key 'arrow left' -> Francky set orientation 90° left
-//    > key 'arrow right' -> Francky set orientation 90° rigth
-//    > key 'A button' -> Francky move forward his orientation
-//
-// -----------------------------------------------
-/*keypress.on('data', function(keyboard) {
-
-
-    // Arrow 'up', to set the orientation "North"
-    if (keyboard == 'a' || keyboard == 'A') {
-
-      console.log(keyboard);
-
-    } else if (keyboard == '\u001B\u005B\u0043') {
-
-      console.log('right');
-
-    } else if (keyboard == '\u001B\u005B\u0044') {
-
-      console.log('left');
-
-    } else {
-
-      console.log('wrong command');
-    }
-
-    /*if (keyboard == '\u001B\u005B\u0041') {
-        //process.stdout.write('up');
-        console.log('up');
-    }
-    if () {
-        process.stdout.write('right');
-    }
-    if (keyboard == '\u001B\u005B\u0042') {
-        process.stdout.write('down');
-    }
-    if (keyboard == '\u001B\u005B\u0044') {
-        process.stdout.write('left');
-    }
-
-    if (keyboard == '\u0003') { process.exit(); }    // ctrl-c
-});*/
-
-
-// Lisen for key pressed event
-// --------------------------------------
-/*process.stdin.on('keypress', function (ch, key) {
-  console.log('got "keypress" : ', key);
-});*/
-
-
-
-
 
 
 //
--- controllers/object/francky.js
+++ controllers/object/francky.js
@@ -15,10 +15,6 @@
 //    - orientation (type : string) - to put the francky orientation
 //      > value "North", "East", "South" or "West"
 //
-//  > This object has 1 methods, Description of the methode jusr below the
-//  declaration of the methods
-//    - addNewFrancky
-//    - moveFrancky
 //
 // -------------------------------------------------------------------------- */
 exports.Francky = function (idFrancky, name, icon, color, positionX, positionY, orientation) {
@@ -29,6 +25,6 @@ exports.Francky = function (idFrancky, name, icon, color, positionX, positionY, 
   this.color = color || "red";
   this.positionX = positionX || 1;
   this.positionY = positionY || 1;
-  this.orientation = orientation || "normal";
+  this.orientation = orientation || "South";
 
 };
